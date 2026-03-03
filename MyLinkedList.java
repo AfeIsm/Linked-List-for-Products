088d8f9957c8214f8f6b5d577617b61a66cddb9d
AfeIsm <afeefsemail@gmail.com>
2026-03-03 15:45:12 -0600

    Clean up in MyLinkedList class
    Removed comments for clarity and conciseness.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -12,17 +12,15 @@ public class MyLinkedList<AnyType extends IDedObject> {
 
     private Node<AnyType> head;
 
-    // Constructor which generates an empty list
     public MyLinkedList() {
         head = null;
     }
 
-    // empties the linked list
     public void makeEmpty() {
         head = null;
     }
 
-    // Get the object with the particular ID, don’t remove; return null if empty or not found
+    
     public AnyType findID(int id) {
         Node<AnyType> cur = head;
         while (cur != null) {
@@ -34,11 +32,9 @@ public class MyLinkedList<AnyType extends IDedObject> {
         return null;
     }
 
-    // insert at front of list or return false if that ID already exists
     public boolean insertAtFront(AnyType x) {
         if (x == null) return false;
 
-        // ID must be unique
         if (findID(x.getID()) != null) {
             return false;
         }
@@ -47,7 +43,6 @@ public class MyLinkedList<AnyType extends IDedObject> {
         return true;
     }
 
-    // delete and return the record at the front or return null if empty
     public AnyType deleteFromFront() {
         if (head == null) return null;
 
@@ -56,11 +51,9 @@ public class MyLinkedList<AnyType extends IDedObject> {
         return removed;
     }
 
-    // find and delete the record with the given ID or return null if not found
     public AnyType delete(int id) {
         if (head == null) return null;
 
-        // if head matches
         if (head.data.getID() == id) {
             return deleteFromFront();
         }
@@ -80,7 +73,6 @@ public class MyLinkedList<AnyType extends IDedObject> {
         return null;
     }
 
-    // print all elements in order; if empty print appropriate message
     public void printAllRecords() {
         if (head == null) {
             System.out.println("List is empty.");
@@ -95,7 +87,6 @@ public class MyLinkedList<AnyType extends IDedObject> {
         }
     }
 
-    // Optional helper (not required) for menu UX
     public boolean isEmpty() {
         return head == null;
     }
